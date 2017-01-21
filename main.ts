85c81115174dace2a77bdbc5fbeb1429b0d24ac8
Peter Menshih <p.menshih@gmail.com>
2017-01-21 22:29:53 +0300

    Добавление Express и Vash

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-console.log("psycho.ru");
+import * as express from "express";
+let app = express();
+
+// каталог шаблонов
+app.set("views", "./views");
+// подключение шаблонизатора Vash
+app.set("view engine", "vash");
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+app.listen(3000, function () {
+});
