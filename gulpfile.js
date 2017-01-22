df18370861c6ee71463d155487378fddc6b1550f
Peter Menshih <p.menshih@gmail.com>
2017-01-22 04:48:46 +0300

    Замена Vash на ECT.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
-var gulp = require("gulp");
-var gulpts = require("gulp-typescript");
+var gulp = require("gulp")
+  ,gulpts = require("gulp-typescript")
+  ,nodemon = require("gulp-nodemon");
 
+// применение глобального конфига TypeSCript
 var tsconfig = gulpts.createProject("tsconfig.json");
 
-gulp.task("ts", function() {
-  return gulp.src("*.ts")
-    .pipe(tsconfig())
-    .pipe(gulp.dest("build"));
+gulp.task("watch", function() {
+  gulp.watch("./*.ts", ["ts"]);
+  gulp.watch("./views/**", ["views"]);
 });
+
+gulp.task("ts", function() {
+  return gulp.src("./*.ts")
+    .pipe(tsconfig())
+    .pipe(gulp.dest("./build"));
+});
+
+gulp.task("views", function() {
+  return gulp.src("./views/**")
+    .pipe(gulp.dest("./build/views"));
+});
+
+gulp.task("server", ["build"], function() {
+  return nodemon({
+  ignore: ["./node_modules/", "./build/"]
+  ,ext: "ect ts"
+  ,script: './build/main.js'
+  ,env: { 'NODE_ENV': 'development' }
+  })
+});
+
+gulp.task("build", ["ts", "views"]);
+
+// gulp.task("default", ["watch"]);
--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
 import * as express from "express";
+import * as ect from "ect";
+
 let app = express();
+let ectRenderer = ect({ root: __dirname + "/views", ext : ".ect" });
 
-// каталог шаблонов
-app.set("views", "./views");
-// подключение шаблонизатора Vash
-app.set("view engine", "vash");
+// подключение шаблонизатора ECT
+app.set("view engine", "ect");
+app.engine("ect", ectRenderer.render);
 
-app.get('/', function (req, res) {
+// маршрутизация
+// главная
+app.get("/", (req, res) => {
   res.render("index");
+});
+// о компании
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+// 404
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+// 500
+app.use((req, res, next) => {
+  res.status(500).render("500");
 });
 
 app.listen(3000, function () {
