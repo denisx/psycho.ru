5cab645b336e52c8fd53a7804b5b89b63b59e892
Peter Menshih <p.menshih@gmail.com>
2017-01-23 01:18:14 +0300

    Перепиливание Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,62 @@
 var gulp = require("gulp")
   ,gulpts = require("gulp-typescript")
   ,sass = require("gulp-sass")
+  ,environments = require("gulp-environments")
+  ,clean = require('gulp-clean')
   ,gls = require('gulp-live-server');
 
+var development = environments.development;
+var production = environments.production;
+
+var dirDebug = "./debug";
+var dirProd = "./release";
+var dirBuild = production() ? dirProd : dirDebug;
+var dirCss = "/css";
+var dirViews = "/views";
+var globStatic = "./frontend/static/**/*";
+var globSass = "./frontend/sass/*.scss";
+var globViews = ["./views/**"];
+var globTS = ["./*.ts"];
 // применение глобального конфига TypeSCript
 var tsconfig = gulpts.createProject("tsconfig.json");
 // константы с путями и глобами
-var dirDebug = "./debug";
-var dirViews = "/views";
-var dirCss = "/css";
-var dirJs = "/js";
+
 var dirFonts = "/fonts";
 var dirMedia = "/media";
 var globFonts = "./frontend/fonts/*"
 var globMedia = "./frontend/media/**/*";
-var globSass = "./frontend/sass/*.scss";
+
 var globJs = "./frontend/ts/*.js";
-var globTS = ["./*.ts"];
-var globViews = ["./views/**"];
+
+gulp.task("clean", function() {
+  return gulp.src(dirBuild, {read: false})
+    .pipe(clean());
+});
+
+gulp.task("static", function() {
+  return gulp.src(globStatic)
+    .pipe(gulp.dest(dirBuild));
+});
+
+gulp.task("sass", function() {
+  return gulp.src(globSass)
+    .pipe(sass())
+    .pipe(gulp.dest(dirBuild + dirCss));
+});
+
+gulp.task("ect", function() {
+  return gulp.src(globViews)
+    .pipe(gulp.dest(dirBuild + dirViews));
+});
+
+gulp.task("tsbe", function() {
+  return gulp.src(globTS)
+    .pipe(tsconfig())
+    .pipe(gulp.dest(dirBuild));
+});
 
 // режим отладки: компиляция всего, запуск сервера с livereload для статики
 gulp.task("debug", function() {
-  // копирование конфига
-  gulp.src("./config_development.json")
-    .pipe(gulp.dest(dirDebug));
-  // копирование шрифтов
-  gulp.src(globFonts)
-    .pipe(gulp.dest(dirDebug + dirFonts));
-  // копирование сторонних js
-  gulp.src(globJs)
-    .pipe(gulp.dest(dirDebug + dirJs));
-  // обработка sass
-  gulp.src(globSass)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(dirDebug + dirCss));
-  // копирование папки media
-  gulp.src(globMedia)
-    .pipe(gulp.dest(dirDebug + dirMedia));
-  // сборка TypeScript бэкенда
-  gulp.src(globTS)
-    .pipe(tsconfig())
-    .pipe(gulp.dest(dirDebug));
-  // сборка вьюх ECT
-  gulp.src(globViews)
-    .pipe(gulp.dest(dirDebug + dirViews));
   // инстанс сервера
   var server = gls("main.js", {
     cwd: dirDebug,
