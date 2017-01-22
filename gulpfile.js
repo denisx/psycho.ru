ed6c2ddad34e49710b742c67929117418f9211b8
Peter Menshih <p.menshih@gmail.com>
2017-01-22 14:45:12 +0300

    Подключение GLS и настройка отладчного таска Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,57 @@
 var gulp = require("gulp")
   ,gulpts = require("gulp-typescript")
-  ,nodemon = require("gulp-nodemon");
+  ,gls = require('gulp-live-server');
 
 // применение глобального конфига TypeSCript
 var tsconfig = gulpts.createProject("tsconfig.json");
+// константы с путями и глобавми
+var dirDebug = "./debug";
+var dirViews = "/views";
+var globTS = ["./*.ts"];
+var globViews = ["./views/**"];
 
-gulp.task("watch", function() {
-  gulp.watch("./*.ts", ["ts"]);
-  gulp.watch("./views/**", ["views"]);
-});
-
+// сборка TypeScript
 gulp.task("ts", function() {
-  return gulp.src("./*.ts")
+  return gulp.src(globTS)
     .pipe(tsconfig())
-    .pipe(gulp.dest("./build"));
+    .pipe(gulp.dest(dirDebug));
 });
 
+// сборка вьюх ECT
 gulp.task("views", function() {
-  return gulp.src("./views/**")
-    .pipe(gulp.dest("./build/views"));
+  return gulp.src(globViews)
+    .pipe(gulp.dest(dirDebug + dirViews));
 });
 
-gulp.task("server", ["build"], function() {
-  return nodemon({
-  ignore: ["./node_modules/", "./build/"]
-  ,ext: "ect ts"
-  ,script: './build/main.js'
-  ,env: { 'NODE_ENV': 'development' }
-  })
+// компиляция того, что нужно компилировать
+gulp.task("compile", ["ts", "views"]);
+
+// режим отладки: компиляция всего, запуск сервера с livereload для статики
+gulp.task("debug", ["compile"], function() {
+  // инстанс сервера
+  var server = gls('main.js', {
+    cwd: dirDebug,
+    env: {NODE_ENV: 'development'}
+  });
+  // запуск
+  server.start();
+  // вотч для вьюх с livereload,
+  // работает с измененными файлами, а не директроями целиком
+  gulp.watch(globViews, function (event) {
+    gulp.src(event.path)
+      // просто копируем измененный файл куда нужно
+      .pipe(gulp.dest(dirDebug + dirViews))
+      // подгружаем изменения в сервер
+      .pipe(server.notify());
+  });
+  // вотч для TS, также работает индивидуально
+  gulp.watch(globTS, function(event) {
+    gulp.src(event.path)
+      // компиляция TS
+      .pipe(tsconfig())
+      // копирование
+      .pipe(gulp.dest(dirDebug))
+      // перезагрузка сервера
+      .on("finish", server.start.bind(server));
+  });
 });
-
-gulp.task("build", ["ts", "views"]);
-
-// gulp.task("default", ["watch"]);
--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
 import * as express from "express";
+// шаблонизатор
 import * as ect from "ect";
 
 let app = express();
-let ectRenderer = ect({ root: __dirname + "/views", ext : ".ect" });
+let ectRenderer = ect({
+  root: __dirname + "/views",
+  ext : ".ect",
+  // кэширование должно быть отключено только для режима отладки,
+  // а не глобально!
+  cache: false });
 
 // подключение шаблонизатора ECT
 app.set("view engine", "ect");
 app.engine("ect", ectRenderer.render);
+// подключение livereload,
+// должно быть только для режима отладки, а не глобально
+app.use(require('connect-livereload')());
 
 // маршрутизация
 // главная
 app.get("/", (req, res) => {
-  res.render("index");
+  var model = { title: "Психология и бизнес" };
+  res.render("index", model);
 });
 // о компании
 app.get("/about", (req, res) => {
@@ -26,5 +36,5 @@ app.use((req, res, next) => {
   res.status(500).render("500");
 });
 
-app.listen(3000, function () {
+app.listen(8205, function () {
 });
