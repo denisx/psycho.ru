fcd469b5cfa6f72be2da296c6136d549e0ac159e
Peter Menshih <p.menshih@gmail.com>
2017-01-26 23:57:19 +0300

    4 основных продукта

---
+++ backend/urls/products/keyhabits.js
@@ -0,0 +1,10 @@
+exports.render = (req, res) => {
+  var model = {
+    title: "Формирование полезных привычек",
+    path: req.path,
+    keywords: "коучинг, персональные планы, тренажеры полезных привычек",
+    description: "",
+    year: new Date().getFullYear()
+  };
+  res.render(`.${req.path}.html`, model);
+}
