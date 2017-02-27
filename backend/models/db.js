78cf76ca933a9f3d74096405d28106064e5f2354
Peter Menshih <p.menshih@gmail.com>
2017-02-27 20:41:01 +0300

    work

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,8 +4,14 @@
 "use strict";
 var Sequelize = require('sequelize'); // ORM
 
+let connectionString = `postgres://${process.env.PSYCHO_DB_USER}@${process.env.PSYCHO_DB_ADDR}:${process.env.PSYCHO_DB_PORT}/${process.env.PSYCHO_DB_DATABASE}`;
+
+if(process.env.PSYCHO_DB_PASS) {
+  connectionString = `postgres://${process.env.PSYCHO_DB_USER}:${process.env.PSYCHO_DB_PASS}@${process.env.PSYCHO_DB_ADDR}:${process.env.PSYCHO_DB_PORT}/${process.env.PSYCHO_DB_DATABASE}`;
+}
+
 var db = {};
-db = new Sequelize(`postgres://${process.env.PSYCHO_DB_USER}:${process.env.PSYCHO_DB_PWD}@${process.env.PSYCHO_DB_ADDR}:${process.env.PSYCHO_DB_PORT}/${process.env.PSYCHO_DB_DATABASE}`,
+db = new Sequelize(connectionString,
   {
     // логирование всех запросов в консоль для продакшена отключим
     logging: process.env.NODE_ENV === "production" ? false : console.log
