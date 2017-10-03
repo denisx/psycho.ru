8549d7428d5b09b83c426dc6dd3ac5976213db1d
Peter Menshih <p.menshih@gmail.com>
2017-10-03 13:28:30 +0300

    Обновление мероприятия "профком"

--- backend/urls/library/article.js
+++ backend/urls/library/article.js
@@ -37,7 +37,7 @@ function renderArticle(a, req, res) {
   if(a.category === 4) {
     model.category = '<span class="item-mark instrumenty-ocenki"><a href="/library/assessment">Оценка персонала</a></span>';
   }
-console.log(dateUpdate)
+
   // применение новых стилей библиотеки для статей
   // отредактированных после 27.02.17
   /* eslint-disable no-magic-numbers */
