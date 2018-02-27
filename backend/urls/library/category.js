c3427d6197e043177347d2ee5b76472e86a08b83
zloy-online <zloy.online@gmail.com>
2018-02-27 18:46:04 +0200

    Выпилил Sequelize из статей и категорий.

--- backend/urls/library/category.js
+++ backend/urls/library/category.js
@@ -2,36 +2,30 @@
  * Отображение статей категории
  */
 "use strict";
-var db = require(`../../models/db.js`);
+var db = require(`../../models/pg_db.js`);
 
 exports.render = (req, res, next) => {
   var catLink = /^\/library\/([\w\d_]{1,50})$/.exec(req.path)[1];
-  // получаем категорию
-  db.libCats.findOne({ where: {link: `${catLink}`}})
-  .then((c) => {
-    // выбираем все её статьи
-    return db.articles.findAll({
-      where: {category: c.id},
-      attributes: ['id', 'title', 'short_descr'],
-      order: 'date_create DESC'
+  let query = `
+    SELECT la.id, la.title, la.short_descr, lc.name
+    FROM library_articles la, library_categories lc
+    WHERE lc.link = $1
+	    AND lc.id = la.category
+    ORDER BY la.date_create DESC;`;
+  db.execute(query, [catLink])
+    .then((a) => {
+      // рендерим страницу
+      var model = {
+        title: a.rows[0].name,
+        keywords: '',
+        description: '',
+        year: new Date().getFullYear(),
+        articles: a.rows
+      };
+      return res.render(`./library/category.html`, model);
     })
-      .then((a) => {
-        // рендерим страницу
-        var model = {
-          title: c.name,
-          keywords: '',
-          description: '',
-          year: new Date().getFullYear(),
-          articles: a
-        };
-        return res.render(`./library/category.html`, model);
-      })
-      .catch((e) => {
-        return next();
-      });
-  })
-  .catch((e) => {
-    return next();
-  });
+    .catch((e) => {
+      return next();
+    });
   return;
 }
