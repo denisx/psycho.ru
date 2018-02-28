8b5c41856c3ed10e11c75c002c7798c8a1dfede0
Menshih Peter <p.menshih@gmail.com>
2018-02-28 10:29:51 +0300

    Фикс форматирования

--- backend/urls/library/category.js
+++ backend/urls/library/category.js
@@ -1,31 +1,35 @@
 /**
  * Отображение статей категории
  */
-"use strict";
-var db = require(`../../models/pg_db.js`);
+'use strict';
+const db = require('../../models/pg_db.js');
 
 exports.render = (req, res, next) => {
-  var catLink = /^\/library\/([\w\d_]{1,50})$/.exec(req.path)[1];
-  let query = `
-    SELECT la.id, la.title, la.short_descr, lc.name
-    FROM library_articles la, library_categories lc
+  let catLink = /^\/library\/([\w\d_]{1,50})$/.exec(req.path)[1],
+    query = `
+    SELECT
+      la.id
+      ,la.title
+      ,la.short_descr
+      ,lc.name
+    FROM library_articles la
+      ,library_categories lc
     WHERE lc.link = $1
 	    AND lc.id = la.category
     ORDER BY la.date_create DESC;`;
   db.execute(query, [catLink])
     .then((a) => {
       // рендерим страницу
-      var model = {
-        title: a.rows[0].name,
-        keywords: '',
+      let model = {
+        articles: a.rows,
         description: '',
+        keywords: '',
+        title: a.rows[0].name,
         year: new Date().getFullYear(),
-        articles: a.rows
       };
-      return res.render(`./library/category.html`, model);
+      return res.render('./library/category.html', model);
     })
     .catch((e) => {
-      return next();
+      return next(e);
     });
-  return;
-}
+};
